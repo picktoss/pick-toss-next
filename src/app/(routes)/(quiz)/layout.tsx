17ccb01389dac6db64d7c756dad3bb40c6d23111
woo-jk <wjk6044@gmail.com>
2024-04-30 18:03:53 +0900

    fix: 배경색 높이 재설정

--- src/app/(routes)/(quiz)/layout.tsx
+++ src/app/(routes)/(quiz)/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="w-full px-20">
+    <div className="w-full px-20 pb-8">
       <HeaderLayout />
       {children}
     </div>
