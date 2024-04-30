ee80cca5304c46782d5bf2806ab47f78029df309
류정우 <evencoding@gmail.com>
2024-05-01 05:45:54 +0900

    fix: layout padding bottom 제거

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,5 +1,6 @@
 import HeaderLayout from '@/components/header-layout'
 import { LeftNavLayout } from '@/components/left-nav-layout'
+import Profile from '@/components/profile'
 import { Viewport } from 'next'
 import { PropsWithChildren } from 'react'
 
@@ -15,10 +16,11 @@ export const viewport: Viewport = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <LeftNavLayout>
-      <div className="mx-auto w-full max-w-[1440px] px-20 pb-8">
+      <div className="mx-auto w-full max-w-[1440px] px-20">
         <HeaderLayout />
         {children}
       </div>
+      <Profile />
     </LeftNavLayout>
   )
 }
