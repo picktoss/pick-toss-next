0cd2177d5f5eb416a74e397b2587561dbeb25bfd
류정우 <evencoding@gmail.com>
2024-05-01 06:10:37 +0900

    fix: layout의 Profile 호출 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import Profile from '@/components/profile'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.variable)}>
         <div className="size-full min-h-screen bg-gray-01">{children}</div>
-        <Profile />
       </body>
     </html>
   )
