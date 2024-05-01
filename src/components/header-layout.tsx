fd8f8b8a4f42ec32d1311f4bdd70958f23f966b4
woo-jk <wjk6044@gmail.com>
2024-05-02 01:58:06 +0900

    feat: 헤더 컴포넌트 프로필 드랍다운 기능 ui 구현

--- src/components/header-layout.tsx
+++ src/components/header-layout.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
 
 const mockData = {
   name: '픽토스',
+  email: 'wjk6044@gmail.com',
   stars: 20,
 }
 
@@ -16,7 +25,7 @@ export default function HeaderLayout() {
             placeholder="문서명, 퀴즈 및 문서 내용 검색"
           />
         </div>
-        <button className="rounded-full p-2 hover:bg-[#ebebeb]  ">
+        <button className="rounded-full p-2 hover:bg-gray-02  ">
           <Image src="/icons/bell.svg" alt="bell" width={24} height={24} />
         </button>
         <div className="flex items-center gap-2 rounded-full bg-[#EFF1F3] px-3 py-0.5">
@@ -24,11 +33,50 @@ export default function HeaderLayout() {
           <span className="font-bold text-gray-07">{mockData.stars}</span>
         </div>
       </div>
-      <div className="flex cursor-pointer items-center gap-[13px]">
-        <div className="size-8 rounded-full bg-orange-04" />
-        <span className="text-sm text-[#818181]">{mockData.name}님</span>
-        <Image src="/icons/arrow-down.svg" alt="arrow-down" width={16} height={16} />
-      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className=" focus:outline-none">
+          <span className="flex items-center gap-[13px] rounded-xl p-1 hover:bg-gray-02">
+            <div className="size-8 rounded-full bg-orange-04" />
+            <span className="text-sm text-[#818181]">{mockData.name}님</span>
+            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={16} height={16} />
+          </span>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            <div className="flex gap-4 rounded-lg bg-blue-01 p-3">
+              <div className="size-10 rounded-full bg-orange-04" />
+              <div>
+                <div className="mb-1 text-sm font-normal text-gray-07">
+                  <strong>{mockData.name}</strong>님
+                </div>
+                <div className="text-sm font-normal text-gray-06">{mockData.email}</div>
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <div className="px-2 py-1 font-bold text-gray-06">
+            <DropdownMenuItem>
+              <span className="text-base">설정</span>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <div className="flex w-full justify-between text-base">
+                <div>문의하기</div>
+                <Image src="/icons/move.svg" alt="move" width={16} height={16} />
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <div className="flex w-full justify-between text-base">
+                <div>정책 및 이용약관</div>
+                <Image src="/icons/move.svg" alt="move" width={16} height={16} />
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <span className="text-base text-notice-red">로그아웃</span>
+            </DropdownMenuItem>
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   )
 }
