2d6158755dab57e07b62a8ad4fe2587439d0418c
류정우 <evencoding@gmail.com>
2024-05-16 07:45:38 +0900

    chore: 불필요한 코드 주석처리

--- src/app/(routes)/(tabs)/repository/[categoryId]/page.tsx
+++ src/app/(routes)/(tabs)/repository/[categoryId]/page.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { Switch } from '@/components/ui/switch'
 import Link from 'next/link'
 import icons from '@/constants/icons'
 
@@ -83,7 +82,7 @@ export default function Category({ params: { categoryId } }: Props) {
             </div>
             <div className="flex items-center gap-12">
               {/* TODO: 서버 API 데이터에 따라 구현 달라질 예정 */}
-              <div className="text-body2-regular text-gray-06">
+              {/* <div className="text-body2-regular text-gray-06">
                 {document.quizCreation === 'PREPARING' ? (
                   <div className="px-[34.5px]">문서 요약 중</div>
                 ) : (
@@ -96,7 +95,7 @@ export default function Category({ params: { categoryId } }: Props) {
                   </div>
                 )}
               </div>
-              <div className="text-body2-regular text-gray-06">{document.createdAt}</div>
+              <div className="text-body2-regular text-gray-06">{document.createdAt}</div> */}
               <DropdownMenu>
                 <DropdownMenuTrigger>
                   <div className="flex size-[25px] items-center justify-center rounded-full hover:bg-gray-02">
