5287232a851921ebe4419f05b0c33651eb715fc3
류정우 <evencoding@gmail.com>
2024-05-19 17:34:03 +0900

    chore: 확장 클래스 빈 객체로 인스턴스화

--- src/app/(routes)/(document)/create/libs/extensions.ts
+++ src/app/(routes)/(document)/create/libs/extensions.ts
@@ -22,22 +22,22 @@ import {
 
 export const extensions = () => [
   new LinkExtension({ autoLink: true }),
-  new BoldExtension(),
-  new StrikeExtension(),
-  new ItalicExtension(),
-  new HeadingExtension(),
-  new BlockquoteExtension(),
+  new BoldExtension({}),
+  new StrikeExtension({}),
+  new ItalicExtension({}),
+  new HeadingExtension({}),
+  new BlockquoteExtension({}),
   new BulletListExtension({ enableSpine: true }),
-  new OrderedListExtension(),
+  new OrderedListExtension({}),
   new ListItemExtension({ priority: ExtensionPriority.High, enableCollapsible: true }),
-  new CodeExtension(),
+  new CodeExtension({}),
   new CodeBlockExtension({ supportedLanguages: [jsx, javascript, typescript] }),
-  new TrailingNodeExtension(),
-  new TableExtension(),
+  new TrailingNodeExtension({}),
+  new TableExtension({}),
   new MarkdownExtension({ copyAsMarkdown: false }),
   /**
    * `HardBreakExtension` allows us to create a newline inside paragraphs.
    * e.g. in a list item
    */
-  new HardBreakExtension(),
+  new HardBreakExtension({}),
 ]
