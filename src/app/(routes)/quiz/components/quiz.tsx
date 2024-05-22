69a37c741deae2f2632b96518d4cf7dc1db81ae6
류정우 <evencoding@gmail.com>
2024-05-22 20:11:48 +0900

    feat: 퀴즈 해설 구현

--- src/app/(routes)/quiz/components/quiz.tsx
+++ src/app/(routes)/quiz/components/quiz.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 import QuizHeader from './quiz-header'
 import AnswerOption, { optionVariants } from './answer-option'
 import { VariantProps } from 'class-variance-authority'
+import Explanation from './explanation'
 
 interface QuizProps {
   quizzes: QuizDTO[]
@@ -18,6 +19,7 @@ export default function Quiz({ quizzes }: QuizProps) {
   const [selectedOrder, setSelectedOrder] = useState<number | null>(null)
   const [showResult, setShowResult] = useState(false)
   const [variants, setVariants] = useState<VariantProps<typeof optionVariants>['variant'][]>([])
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null)
 
   const curQuiz = quizzes[quizIndex]
 
@@ -31,6 +33,7 @@ export default function Quiz({ quizzes }: QuizProps) {
 
   const onSelectAnswer = (order: number) => {
     setSelectedOrder(order)
+    setIsCorrect(curQuiz.options[order] === curQuiz.answer)
 
     setTimeout(() => {
       setShowResult(true)
@@ -65,8 +68,8 @@ export default function Quiz({ quizzes }: QuizProps) {
         <QuizIntro quizzes={quizzes} />
       ) : (
         <div>
-          <QuizHeader className="mb-[32px]" />
-          <div className="mb-[24px]">
+          <QuizHeader className="mb-[32px] px-[20px]" />
+          <div className="mb-[24px] px-[20px]">
             <div className="w-full overflow-hidden rounded-[12px]">
               <div className="relative h-[8px] *:h-[8px]">
                 <div className="bg-gray-02" />
@@ -84,7 +87,7 @@ export default function Quiz({ quizzes }: QuizProps) {
             </div>
           </div>
           {curQuiz.quizType === 'MULTIPLE_CHOICE' ? (
-            <div className="flex flex-col gap-[20px]">
+            <div className="flex flex-col gap-[20px] px-[20px]">
               {curQuiz.options.map((option, idx) => (
                 <AnswerOption
                   key={idx}
@@ -99,6 +102,19 @@ export default function Quiz({ quizzes }: QuizProps) {
           ) : (
             <div></div>
           )}
+          {showResult ? (
+            <Explanation
+              isCorrect={isCorrect!}
+              correctItem={String.fromCharCode(
+                65 + curQuiz.options.findIndex((option) => curQuiz.answer === option)
+              )}
+              explanation={
+                '윌리엄 홀만 교수가 제시한 신식품 명명법의 주요 기준은 다섯 가지로, 소비자가 세포 기반 제품을 기존 제품과 구별할 수 있어야 하며, 전통적인 생선, 조개류, 소고기 또는 가금류에 알레르기가 있는 사람들이 세포 기반 제품을 잠재적 알레르겐으로 식별할 수 있도록 해야 합니다. 또한, 세포 기반 제품이나 기존 제품을 비하하지 않는 이름이어야 하며, 제품이 안전하고 건강하며 영양가 있다는 사실과 일치하지 않는 생각, 이미지 또는 감정을 불러일으키지 않는 중립적인 이름이어야 합니다. 마지막으로, 소비자가 제품을 식별할 수 있는 적절한 용어로 인식해야 한다고 합니다.'
+              }
+              next={() => {}}
+              className="mt-[48px]"
+            />
+          ) : null}
         </div>
       )}
     </div>
