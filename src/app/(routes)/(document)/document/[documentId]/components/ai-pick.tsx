c8d682da14f9703afb8791d960542ee4b0b98899
류정우 <evencoding@gmail.com>
2024-06-05 19:55:53 +0900

    feat: ai-pick unprocessed UI

--- src/app/(routes)/(document)/document/[documentId]/components/ai-pick.tsx
+++ src/app/(routes)/(document)/document/[documentId]/components/ai-pick.tsx
@@ -24,6 +24,7 @@ interface Props {
     question: string
     answer: string
   }[]
+  status: 'UNPROCESSED' | 'PROCESSED' | 'PROCESSING' | 'KEYPOINT_UPDATE_POSSIBLE'
 }
 
 const slideInOutVariants = {
@@ -44,7 +45,7 @@ const slideInOutVariants = {
   },
 }
 
-export function AiPick({ keyPoints }: Props) {
+export function AiPick({ keyPoints, status }: Props) {
   const isDesktop = useMediaQuery('(min-width: 1024px)')
   const { isPickOpen, setIsPickOpen } = useDocumentDetailContext()
   const [showToggle, setShowToggle] = useState(false)
@@ -94,7 +95,7 @@ export function AiPick({ keyPoints }: Props) {
         <AnimatePresence>
           {isPickOpen && (
             <motion.div
-              className="top-0 flex h-screen w-[420px] flex-col gap-[6px] overflow-scroll bg-white"
+              className="relative top-0 flex h-screen w-[420px] flex-col overflow-scroll bg-white"
               initial="initial"
               animate="animate"
               exit="exit"
@@ -102,14 +103,62 @@ export function AiPick({ keyPoints }: Props) {
               onAnimationStart={() => setShowToggle(false)}
               onAnimationComplete={() => setShowToggle(true)}
             >
-              <div>
-                <div className="flex h-[80px] items-center gap-[24px] px-[19px]">
-                  <h3 className="text-h2-bold text-gray-08">AI pick</h3>
-                  <Button className="h-[36px] w-[120px] rounded-full">다시 생성하기</Button>
+              <div className="flex flex-col gap-[15px] pt-[23px]">
+                <div className="flex h-[48px] items-center px-[19px]">
+                  <h3 className="text-h3-bold text-gray-08">AI pick</h3>
+                </div>
+
+                <div className="px-[10px]">
+                  <PickBanner status={status} />
                 </div>
               </div>
 
-              <div className="flex flex-col gap-[36px] px-[16px] pb-[80px]">
+              <div className="flex-1 py-[40px]">
+                {status === 'UNPROCESSED' ? (
+                  <div className="relative h-full">
+                    <div className="absolute bottom-1/2 flex w-full flex-col items-center gap-[24px] text-center text-text-medium text-gray-08">
+                      <p>
+                        AI pick으로 퀴즈 내용을 선정하고
+                        <br />
+                        노트 요약을 확인해보세요
+                      </p>
+                      <Button variant="gradation" size="sm" className="w-fit gap-[4px]">
+                        <StarsIcon />
+                        pick 시작
+                      </Button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="flex-1 overflow-scroll px-[20px]">
+                    <Accordion type="multiple" className="flex flex-col px-[16px] py-[7px]">
+                      {keyPoints.map((keyPoint, index) => (
+                        <AccordionItem value={keyPoint.id.toString()} key={keyPoint.id}>
+                          <AccordionTrigger
+                            className="flex items-center justify-between"
+                            chevronDownIcon={
+                              <div className="flex size-[24px] items-center justify-center rounded-full bg-blue-02">
+                                <ChevronDown size={16} color="#7095F8" strokeWidth={3} />
+                              </div>
+                            }
+                          >
+                            <div className="flex flex-1 gap-1">
+                              <span className="text-body2-bold text-blue-06">{index + 1}</span>
+                              <span className="text-left text-body2-medium text-gray-09">
+                                {keyPoint.question}
+                              </span>
+                            </div>
+                          </AccordionTrigger>
+                          <AccordionContent className="px-[14px] text-body2-regular text-gray-08">
+                            {keyPoint.answer}
+                          </AccordionContent>
+                        </AccordionItem>
+                      ))}
+                    </Accordion>
+                  </div>
+                )}
+              </div>
+
+              {/* <div className="flex flex-col gap-[36px] px-[16px] pb-[80px]">
                 <div className="flex items-center justify-between rounded-[8px] bg-blue-01 py-[16px] pl-[14px] pr-[18px]">
                   <div className="flex items-center gap-[8px]">
                     <Image src={icons.pin} width={24} height={24} alt="" />
@@ -146,7 +195,7 @@ export function AiPick({ keyPoints }: Props) {
                     ))}
                   </Accordion>
                 </div>
-              </div>
+              </div> */}
             </motion.div>
           )}
         </AnimatePresence>
@@ -212,6 +261,45 @@ export function AiPick({ keyPoints }: Props) {
   )
 }
 
+function PickBanner({
+  status,
+}: {
+  status: 'UNPROCESSED' | 'PROCESSED' | 'PROCESSING' | 'KEYPOINT_UPDATE_POSSIBLE'
+}) {
+  return (
+    <SwitchCase
+      value={status}
+      caseBy={{
+        PROCESSED: (
+          <div className="flex items-center justify-between rounded-[8px] bg-blue-01 py-[16px] pl-[14px] pr-[18px]">
+            <div className="flex items-center gap-[8px]">
+              <Image src={icons.pin} width={24} height={24} alt="" />
+              <div className="text-text-bold text-blue-06">
+                픽토스 AI의 질문을 통해 내용을 돌아보세요
+              </div>
+            </div>
+            {/* <div className="text-small1-regular text-orange-06 underline">가이드 보기</div> */}
+          </div>
+        ),
+        KEYPOINT_UPDATE_POSSIBLE: (
+          <div className="flex items-center justify-between rounded-[8px] bg-gray-01 px-[16px] py-[21px]">
+            <div className="flex items-center gap-[8px]">
+              <GradientStarsIcon />
+              <div className="text-text-medium text-gray-08">
+                퀴즈와 요약에 수정한 내용을 반영해보세요
+              </div>
+            </div>
+            <div role="button" className="p-[5px] text-small1-bold text-orange-06">
+              pick 다시하기
+            </div>
+          </div>
+        ),
+      }}
+      defaultComponent={null}
+    />
+  )
+}
+
 function ChevronRightIcon() {
   return (
     <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -236,6 +324,60 @@ function ChevronLeftIcon() {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
+    </svg>
+  )
+}
+
+function StarsIcon() {
+  return (
+    <svg width="13" height="16" viewBox="0 0 13 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9.5251 0.951505C9.68269 0.682832 10.0415 0.682832 10.1991 0.951505L10.8087 1.99086C10.8344 2.03457 10.8669 2.07294 10.9048 2.10419L11.8401 2.87501C12.0528 3.0503 12.0528 3.3997 11.8401 3.57499L10.9048 4.34581C10.8669 4.37706 10.8344 4.41544 10.8087 4.45915L10.1991 5.4985C10.0415 5.76717 9.68269 5.76717 9.5251 5.4985L8.91549 4.45915C8.88986 4.41544 8.85732 4.37706 8.81941 4.34581L7.88413 3.57499C7.67144 3.3997 7.67144 3.0503 7.88413 2.87501L8.81941 2.10419C8.85732 2.07294 8.88986 2.03457 8.91549 1.99085L9.5251 0.951505Z"
+        fill="white"
+      />
+      <path
+        d="M5.34395 5.86637C5.63885 5.37788 6.31037 5.37788 6.60527 5.86637L7.74605 7.7561C7.79403 7.83557 7.85491 7.90535 7.92586 7.96216L9.67609 9.36365C10.0741 9.68237 10.0741 10.3176 9.67609 10.6364L7.92586 12.0378C7.85491 12.0947 7.79403 12.1644 7.74605 12.2439L6.60526 14.1336C6.31037 14.6221 5.63885 14.6221 5.34395 14.1336L4.20316 12.2439C4.15519 12.1644 4.0943 12.0947 4.02335 12.0378L2.27313 10.6364C1.8751 10.3176 1.8751 9.68237 2.27313 9.36365L4.02335 7.96216C4.0943 7.90535 4.15519 7.83557 4.20317 7.7561L5.34395 5.86637Z"
+        fill="white"
+      />
+    </svg>
+  )
+}
+
+function GradientStarsIcon() {
+  return (
+    <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11.4297 1.04161C11.6188 0.719203 12.0494 0.719203 12.2386 1.04161L12.9701 2.28883C13.0008 2.34128 13.0399 2.38733 13.0854 2.42483L14.2077 3.34981C14.4629 3.56017 14.4629 3.97944 14.2077 4.1898L13.0854 5.11478C13.0399 5.15228 13.0008 5.19833 12.9701 5.25078L12.2385 6.498C12.0494 6.82041 11.6188 6.82041 11.4297 6.498L10.6982 5.25078C10.6674 5.19833 10.6284 5.15228 10.5829 5.11478L9.46057 4.1898C9.20533 3.97944 9.20533 3.56017 9.46057 3.34981L10.5829 2.42483C10.6284 2.38733 10.6674 2.34128 10.6982 2.28883L11.4297 1.04161Z"
+        fill="url(#paint0_linear_3576_1510)"
+      />
+      <path
+        d="M6.41235 6.93945C6.76623 6.35326 7.57206 6.35326 7.92593 6.93945L9.29487 9.20712C9.35244 9.30249 9.42551 9.38622 9.51065 9.4544L11.6109 11.1362C12.0885 11.5186 12.0885 12.281 11.6109 12.6634L9.51065 14.3452C9.42551 14.4134 9.35244 14.4971 9.29487 14.5925L7.92593 16.8602C7.57205 17.4464 6.76623 17.4464 6.41235 16.8602L5.04341 14.5925C4.98584 14.4971 4.91278 14.4134 4.82763 14.3452L2.72736 12.6634C2.24973 12.281 2.24973 11.5186 2.72736 11.1362L4.82763 9.45439C4.91278 9.38622 4.98584 9.30249 5.04341 9.20712L6.41235 6.93945Z"
+        fill="url(#paint1_linear_3576_1510)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_3576_1510"
+          x1="5"
+          y1="6"
+          x2="-1.443"
+          y2="9.443"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF8428" />
+          <stop offset="1" stopColor="#93B0FF" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_3576_1510"
+          x1="5"
+          y1="6"
+          x2="-1.443"
+          y2="9.443"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF8428" />
+          <stop offset="1" stopColor="#93B0FF" />
+        </linearGradient>
+      </defs>
     </svg>
   )
 }
