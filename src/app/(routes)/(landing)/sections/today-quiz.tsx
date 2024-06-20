3c8f3b1d7c27d52609e6a56d0625f6a53f1489db
류정우 <evencoding@gmail.com>
2024-06-20 13:37:02 +0900

    feat: landing today quiz continuous quiz dates desktop

--- src/app/(routes)/(landing)/sections/today-quiz.tsx
+++ src/app/(routes)/(landing)/sections/today-quiz.tsx
@@ -37,17 +37,30 @@ export function TodayQuiz() {
         </div>
       </Section>
 
-      <section className="mt-[172px] hidden bg-orange-gradient xl:block">
+      <section className="mt-[90px] hidden bg-orange-gradient xl:block">
         <div className="mx-auto flex max-w-[1032px] *:shrink-0">
-          <TodayQuizScreenIcon svgKey="desktop-today-quiz-screen" className="h-[723px] w-[537px]" />
-          <div className="ml-[-40px] pt-[157px]">
-            <FireworkIcon svgKey="desktop-firework" className="ml-[-12px] h-[68.9px] w-[74px]" />
+          <TodayQuizScreenIcon
+            svgKey="desktop-today-quiz-screen"
+            className="ml-[-50px] h-[900px] w-[660px]"
+          />
+          <div className="ml-[-60px] pt-[230px]">
+            <FireworkIcon svgKey="desktop-firework" className="ml-[-14px] h-[68.9px] w-[74px]" />
             <div className="mt-[6px] text-h3-bold text-orange-06">오늘의 퀴즈</div>
             <div className="mt-[16px] text-h1 text-gray-09">
-              나의 노트에서 만들어진
+              매일 나의 노트로부터 자동으로
               <br />
-              다양한 유형의 퀴즈를 풀 수 있어요
+              만들어진 퀴즈를 풀어보세요
             </div>
+          </div>
+        </div>
+
+        <div className="mt-[20px] flex flex-col items-center">
+          <TrophyIcon svgKey="desktop-trophy" className="h-[91px] w-[79.4px]" />
+          <div className="mt-[34px]">
+            <ContinuousQuizDatesIcon className="h-[75px] w-[413px]" />
+          </div>
+          <div className="mt-[41px] text-h3-bold text-gray-09">
+            꾸준히 퀴즈를 풀면, 어느새 마스터가 되어 있을 거예요
           </div>
         </div>
       </section>
