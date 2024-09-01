a5f1960c39167d606152d9bdfc25dad9c44cc0cc
류정우 <evencoding@gmail.com>
2024-09-01 13:42:19 +0900

    refactor: api-client

--- src/actions/fetchers/key-point/toggle-bookmark/index.ts
+++ src/actions/fetchers/key-point/toggle-bookmark/index.ts
@@ -1,26 +1,20 @@
+'use server'
+
 import { apiClient } from '@/actions/api-client'
 import { API_ENDPOINT } from '@/actions/endpoints'
-import { PrivateRequest } from '@/actions/types'
 
-interface ToggleBookmarkParams extends PrivateRequest {
+interface ToggleBookmarkParams extends NextFetchRequestConfig {
   keypointId: number
   bookmark: boolean
 }
 
 export interface ToggleBookmarkResponse {}
 
-export const toggleBookmark = async ({
-  keypointId,
-  bookmark,
-  accessToken,
-}: ToggleBookmarkParams) => {
+export const toggleBookmark = async ({ keypointId, bookmark }: ToggleBookmarkParams) => {
   return await apiClient.fetch<ToggleBookmarkResponse>({
     endpoint: API_ENDPOINT.keyPoint.patchBookmark(keypointId),
     body: {
       bookmark,
-    },
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
     },
   })
 }
