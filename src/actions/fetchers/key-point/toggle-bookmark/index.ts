d98eabffa5baf9883f9cfa3eed8f22d4549266c9
류정우 <evencoding@gmail.com>
2024-09-01 13:56:02 +0900

    fix: build error

--- src/actions/fetchers/key-point/toggle-bookmark/index.ts
+++ src/actions/fetchers/key-point/toggle-bookmark/index.ts
@@ -4,15 +4,15 @@ import { apiClient } from '@/actions/api-client'
 import { API_ENDPOINT } from '@/actions/endpoints'
 
 interface ToggleBookmarkParams extends NextFetchRequestConfig {
-  keypointId: number
+  keyPointId: number
   bookmark: boolean
 }
 
 export interface ToggleBookmarkResponse {}
 
-export const toggleBookmark = async ({ keypointId, bookmark }: ToggleBookmarkParams) => {
+export const toggleBookmark = async ({ keyPointId, bookmark }: ToggleBookmarkParams) => {
   return await apiClient.fetch<ToggleBookmarkResponse>({
-    endpoint: API_ENDPOINT.keyPoint.patchBookmark(keypointId),
+    endpoint: API_ENDPOINT.keyPoint.patchBookmark(keyPointId),
     body: {
       bookmark,
     },
