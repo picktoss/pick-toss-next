9d648afd9ba98baf5fba8e7a311dcda8c12935f3
류정우 <evencoding@gmail.com>
2024-09-26 22:28:37 +0900

    feat: ref를 받을 수 있도록 수정

--- src/shared/components/text.tsx
+++ src/shared/components/text.tsx
@@ -1,4 +1,4 @@
-import React, { ElementType, ComponentPropsWithoutRef } from 'react'
+import React, { ElementType, ComponentPropsWithoutRef, forwardRef } from 'react'
 import { cn } from '../lib/utils'
 
 type Typography =
@@ -28,54 +28,57 @@ type TextProps<T extends ElementType> = {
   as?: T
 } & Omit<ComponentPropsWithoutRef<T>, 'as' | 'typography'>
 
-const Text = <T extends ElementType = 'div'>({
-  typography,
-  className,
-  as,
-  children,
-  ...props
-}: TextProps<T>) => {
-  const Component = as || 'div'
+const Text = forwardRef(
+  <T extends ElementType = 'div'>(
+    { typography, className, as, children, ...props }: TextProps<T>,
+    ref?: React.ComponentPropsWithRef<T>['ref']
+  ) => {
+    const Component = as || 'div'
 
-  return (
-    <Component
-      className={cn(
-        {
-          'text-[36px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'hero',
-          'text-[30px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title1',
-          'text-[24px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title2',
-          'text-[20px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title3',
-          'text-[18px] font-bold leading-[150%] tracking-[-0.02em]':
-            typography === 'subtitle1-bold',
-          'text-[16px] font-bold leading-[150%] tracking-[-0.02em]':
-            typography === 'subtitle2-bold',
-          'text-[16px] font-medium leading-[150%] tracking-[-0.02em]':
-            typography === 'subtitle2-medium',
-          'text-[14px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'text1-bold',
-          'text-[14px] font-medium leading-[150%] tracking-[-0.02em]':
-            typography === 'text1-medium',
-          'text-[14px] font-normal leading-[150%] tracking-[-0.02em]':
-            typography === 'text1-regular',
-          'text-[12px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'text2-bold',
-          'text-[12px] font-medium leading-[150%] tracking-[-0.02em]':
-            typography === 'text2-medium',
-          'text-[10px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'caption-bold',
-          'text-[10px] font-medium leading-[150%] tracking-[-0.02em]':
-            typography === 'caption-medium',
-          'text-[18px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button1',
-          'text-[16px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button2',
-          'text-[14px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button3',
-          'text-[14px] font-medium leading-normal tracking-[-0.02em]': typography === 'button4',
-          'text-[12px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button5',
-          'text-[20px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'question',
-        },
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </Component>
-  )
-}
+    return (
+      <Component
+        ref={ref}
+        className={cn(
+          {
+            'text-[36px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'hero',
+            'text-[30px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title1',
+            'text-[24px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title2',
+            'text-[20px] font-bold leading-[120%] tracking-[-0.02em]': typography === 'title3',
+            'text-[18px] font-bold leading-[150%] tracking-[-0.02em]':
+              typography === 'subtitle1-bold',
+            'text-[16px] font-bold leading-[150%] tracking-[-0.02em]':
+              typography === 'subtitle2-bold',
+            'text-[16px] font-medium leading-[150%] tracking-[-0.02em]':
+              typography === 'subtitle2-medium',
+            'text-[14px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'text1-bold',
+            'text-[14px] font-medium leading-[150%] tracking-[-0.02em]':
+              typography === 'text1-medium',
+            'text-[14px] font-normal leading-[150%] tracking-[-0.02em]':
+              typography === 'text1-regular',
+            'text-[12px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'text2-bold',
+            'text-[12px] font-medium leading-[150%] tracking-[-0.02em]':
+              typography === 'text2-medium',
+            'text-[10px] font-bold leading-[150%] tracking-[-0.02em]':
+              typography === 'caption-bold',
+            'text-[10px] font-medium leading-[150%] tracking-[-0.02em]':
+              typography === 'caption-medium',
+            'text-[18px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button1',
+            'text-[16px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button2',
+            'text-[14px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button3',
+            'text-[14px] font-medium leading-normal tracking-[-0.02em]': typography === 'button4',
+            'text-[12px] font-semibold leading-normal tracking-[-0.02em]': typography === 'button5',
+            'text-[20px] font-bold leading-[150%] tracking-[-0.02em]': typography === 'question',
+          },
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </Component>
+    )
+  }
+)
+
+Text.displayName = 'Text'
 
 export default Text
