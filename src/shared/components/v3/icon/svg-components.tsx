8cf72893b0502dcac776303758f0170cf94d6bbb
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-10 18:09:08 +0900

    fix: switch 컴포넌트 수정, svg-components icon 추가

--- src/shared/components/v3/icon/svg-components.tsx
+++ src/shared/components/v3/icon/svg-components.tsx
@@ -1165,6 +1165,22 @@ export const Asterisk = ({ ...props }) => {
   )
 }
 
+// (. 가운뎃점)
+export const MiddleDot = ({ ...props }) => {
+  return (
+    <svg
+      width="3"
+      height="3"
+      viewBox="0 0 3 3"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle cx="1.5" cy="1.5" r="1.5" fill="#D3DCE4" />
+    </svg>
+  )
+}
+
 // GNB
 export const Person = ({ ...props }) => {
   const { fill, ...rest } = props
