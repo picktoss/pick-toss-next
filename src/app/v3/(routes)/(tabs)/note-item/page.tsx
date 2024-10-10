7ceafd4454a125c36094ac30549065165e3a2ff8
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-10 18:10:41 +0900

    feat: 퀴즈 노트 페이지 depth1

---
+++ src/app/v3/(routes)/(tabs)/note-item/page.tsx
@@ -0,0 +1,11 @@
+import Note from '@/views/v3/note/[noteId]'
+
+const Page = () => {
+  return (
+    <>
+      <Note />
+    </>
+  )
+}
+
+export default Page
---
+++ src/views/v3/note/[noteId]/components/pick-banner.tsx
@@ -0,0 +1,24 @@
+import icons from '@/constants/icons'
+import Text from '@/shared/components/text'
+import Icon from '@/shared/components/v3/icon'
+import Image from 'next/image'
+
+// PickBanner 컴포넌트
+const PickBanner = () => {
+  return (
+    <div className="flex-center mb-[24px] h-[44px] w-[343px] max-w-full gap-[4px] rounded-[12px] bg-background-container-03">
+      <Text typography="text1-medium" className="flex-center gap-[16px] text-text-info">
+        <Text className="flex items-center">
+          <Image src={icons.pin} alt="" width={16} height={16} className="mr-[2.5px]" />
+          내가 어려워했던 문제는?
+        </Text>
+        <Text className="font-bold">
+          복습 <i>p</i>ick
+        </Text>
+      </Text>
+      <Icon name="chevron-down" className="text-icon-tertiary" />
+    </div>
+  )
+}
+
+export default PickBanner
