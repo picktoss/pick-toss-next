eb7fc7c1473bcb45e0842ac51d8459423dbc39af
류정우 <evencoding@gmail.com>
2024-10-14 22:57:29 +0900

    fix: remove version 2

--- src/app/(routes)/(landing)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export default function LandingLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-md xl:max-w-none">{children}</div>
-    </div>
-  )
-}
