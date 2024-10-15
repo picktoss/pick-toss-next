f4908a19a760e04a84d67a2eea4ee7e674e0d022
류정우 <evencoding@gmail.com>
2024-10-15 22:05:55 +0900

    fix: build error3

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation'
 export default async function AdminLayout({ children }: PropsWithChildren) {
   const session = await auth()
   if (session?.user.dto.role !== 'ROLE_ADMIN') {
-    return notFound()
+    notFound()
   }
 
   return children
