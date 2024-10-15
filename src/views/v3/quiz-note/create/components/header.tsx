84fb848fa2f28f2ef463ac2b9691ea064a86fa30
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-15 13:02:43 +0900

    chore: 파일 구조 수정

---
+++ src/views/v3/quiz-note/create/components/header.tsx
@@ -0,0 +1,22 @@
+import Text from '@/shared/components/text'
+import Icon from '@/shared/components/v3/icon'
+
+const Header = () => {
+  return (
+    <header className="fixed right-1/2 top-0 z-20 flex h-[54px] w-full max-w-[430px] translate-x-1/2 border-b border-border-divider bg-background-base-01 px-[16px] transition-all">
+      <div className="flex size-full items-center justify-between">
+        <div className="flex items-center">
+          <button>
+            <Icon name="cancel" className="size-[24px]" />
+          </button>
+        </div>
+        <div className="flex items-center text-text1-medium">
+          <Text className="mr-[12px] text-text-sub">폴더</Text>
+          <div className="rounded-full bg-background-base-02 px-[16px] py-[5px]">📊 전공 공부</div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/views/v3/quiz-note/create/editor/index.tsx
+++ src/views/v3/quiz-note/create/editor/index.tsx
@@ -5,6 +5,7 @@ import Text from '@/shared/components/text'
 import dynamic from 'next/dynamic'
 import Loading from '@/shared/components/loading'
 import CreateQuizButton from '../components/create-quiz-button'
+import Header from '../components/header'
 
 // Remirror가 브라우저 전용 라이브러리라 서버에서 렌더링될 때 오류 발생, 이를 해결하기 위해 dynamic import를 사용
 const VisualEditor = dynamic(() => import('./components/visual-editor'), {
@@ -14,25 +15,29 @@ const VisualEditor = dynamic(() => import('./components/visual-editor'), {
 
 const CreateWithEditor = () => {
   return (
-    <CreateNoteProvider>
-      <TitleInput />
+    <>
+      <Header />
 
-      <div className="sticky top-[54px] z-10 flex items-center justify-between bg-background-base-02 px-[16px] py-[11px]">
-        <div className="flex items-center">
-          <Icon name="info" className="mr-[4px] size-[16px]" />
-          <Text as="span" typography="text2-medium" className="text-text-caption">
-            최소 500자, 최대 15000자 입력 가능
+      <CreateNoteProvider>
+        <TitleInput />
+
+        <div className="sticky top-[54px] z-10 flex items-center justify-between bg-background-base-02 px-[16px] py-[11px]">
+          <div className="flex items-center">
+            <Icon name="info" className="mr-[4px] size-[16px]" />
+            <Text as="span" typography="text2-medium" className="text-text-caption">
+              최소 500자, 최대 15000자 입력 가능
+            </Text>
+          </div>
+          <Text typography="text1-medium" className="text-text-secondary">
+            <span className="text-text-caption">{0}</span> / 15000
           </Text>
         </div>
-        <Text typography="text1-medium" className="text-text-secondary">
-          <span className="text-text-caption">{0}</span> / 15000
-        </Text>
-      </div>
 
-      <VisualEditor />
+        <VisualEditor />
 
-      <CreateQuizButton />
-    </CreateNoteProvider>
+        <CreateQuizButton />
+      </CreateNoteProvider>
+    </>
   )
 }
 
