4e2972a237c91394ac8335fcc60e902f4b9ea828
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-19 12:18:10 +0900

    feat: header + main container

---
+++ src/views/profile/index.tsx
@@ -0,0 +1,15 @@
+import Header from './components/header'
+
+const Profile = () => {
+  return (
+    <>
+      <Header />
+
+      <main className="h-[calc(100dvh-54px-88px)] w-full overflow-y-auto px-[16px]">
+        <div></div>
+      </main>
+    </>
+  )
+}
+
+export default Profile
