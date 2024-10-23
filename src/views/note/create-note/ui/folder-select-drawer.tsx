ea321aecf89ced65694148d7c93bd3fd89fc4b7d
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-23 18:29:47 +0900

    fix: 퀴즈노트 drawer width 조정 및 css 수정

--- src/views/note/create-note/ui/folder-select-drawer.tsx
+++ src/views/note/create-note/ui/folder-select-drawer.tsx
@@ -35,6 +35,36 @@ const FolderSelectDrawer = () => {
       folderName: '📊 전공 공부',
       noteAmount: 15,
     },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
+    {
+      id: '2',
+      folderName: '📊 전공 공부',
+      noteAmount: 15,
+    },
   ]
 
   return (
@@ -45,8 +75,11 @@ const FolderSelectDrawer = () => {
         </button>
       </DrawerTrigger>
 
-      <DrawerContent className="flex h-[85dvh] flex-col rounded-t-[16px]">
-        <div className="mt-[16px] flex items-center justify-between px-[18px]">
+      <DrawerContent
+        overlayProps={{ className: 'max-w-mobile mx-auto' }}
+        className="mx-auto flex h-[80dvh] max-w-mobile flex-col rounded-t-[20px]"
+      >
+        <div className="mt-[16px] flex w-full items-center justify-between px-[18px]">
           <DrawerTitle className="text-title3">폴더 선택</DrawerTitle>
           <DrawerClose asChild>
             <button className="text-text-primary" onClick={() => setIsOpen(false)}>
@@ -55,8 +88,8 @@ const FolderSelectDrawer = () => {
           </DrawerClose>
         </div>
 
-        <div className="flex h-fit flex-col bg-background-base-01">
-          <div className="border-b border-border-divider">
+        <div className="flex h-[calc(100%-58px)] w-full flex-col bg-background-base-01">
+          <div className="flex h-[calc(100%-58px)] flex-col border-b border-border-divider">
             <DrawerTitle className="mt-[24px] flex items-center justify-between px-[18px]">
               <Text as="span" typography="subtitle2-medium">
                 전체 노트
@@ -65,7 +98,7 @@ const FolderSelectDrawer = () => {
                 노트 30개
               </Text>
             </DrawerTitle>
-            <div className="mb-[11px] mt-[9px] flex max-h-[50dvh] flex-col overflow-y-auto px-[18px]">
+            <div className="mb-[11px] mt-[9px] flex grow flex-col overflow-y-auto px-[18px]">
               {/* 폴더 개수만큼 렌더링 */}
               {folderList.map((folder) => (
                 <button key={folder.id} className="flex items-center justify-between py-[10px]">
