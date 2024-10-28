690a8ebd18d2cd4de3461cafdaae025c55580b18
nageuna922@gmail.com <nageuna922@gmail.com>
2024-10-28 16:49:58 +0900

    refactor: 뒤로가기 버튼 공통 컴포넌트로 분리

--- src/views/profile/notification/index.tsx
+++ src/views/profile/notification/index.tsx
@@ -1,23 +1,16 @@
-'use client'
-
-import Icon from '@/shared/components/icon'
 import Text from '@/shared/components/ui/text'
-import { useRouter } from 'next/navigation'
 import { NotificationProvider } from './context/notification-context'
 import NotificationControlArea from './components/notification-control-area'
+import GoBackButton from '@/shared/components/go-back-button'
 
 const Notification = () => {
-  const router = useRouter()
-
   // 임시
   const isKakaoUser = true
 
   return (
     <>
       <header className="relative flex h-[54px] w-full items-center bg-background-base-01 px-[16px]">
-        <button className="justify-self-start" onClick={() => router.back()}>
-          <Icon name="arrow-left" className="size-[24px]" />
-        </button>
+        <GoBackButton />
         <Text typography="subtitle2-medium" className="center">
           알림 설정
         </Text>
