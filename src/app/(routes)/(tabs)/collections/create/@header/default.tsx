90b8b8f2b32dc7720b989b0e0da31b02f66bdb48
류정우 <evencoding@gmail.com>
2024-11-04 16:47:28 +0900

    fix: cancel 버튼 추상화

--- src/app/(routes)/(tabs)/collections/create/@header/default.tsx
+++ src/app/(routes)/(tabs)/collections/create/@header/default.tsx
@@ -1,17 +1,10 @@
-'use client'
-
-import Icon from '@/shared/components/custom/icon'
+import GoBackButton from '@/shared/components/custom/go-back-button'
 import Text from '@/shared/components/ui/text'
-import { useRouter } from 'next/navigation'
 
 const Header = () => {
-  const router = useRouter()
-
   return (
     <header className="sticky top-0 flex h-[54px] shrink-0 items-center bg-white px-[16px]">
-      <button onClick={() => router.back()}>
-        <Icon name="cancel" />
-      </button>
+      <GoBackButton icon="cancel" />
       <div className="absolute right-1/2 translate-x-1/2">
         <Text as="h1" typography="subtitle2-medium">
           컬렉션 만들기
