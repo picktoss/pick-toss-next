b40617b0c07e7606824ef834e59aa8c6dc3b908a
nageuna922@gmail.com <nageuna922@gmail.com>
2024-11-05 16:56:51 +0900

    feat: main 페이지 layout

--- src/app/(routes)/main/page.tsx
+++ src/app/(routes)/main/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => {
-  return <div className="">asd</div>
+  return (
+    <main className="h-[calc(100dvh-54px-88px)] w-full overflow-y-auto bg-background-base-02 px-[16px]"></main>
+  )
 }
 
 export default Home
