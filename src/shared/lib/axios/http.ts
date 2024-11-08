bdb7f8f98f129d16a5076995b1fee0ccaaf4b870
류정우 <evencoding@gmail.com>
2024-11-09 01:20:16 +0900

    feat: fetch-today-quiz

--- src/shared/lib/axios/http.ts
+++ src/shared/lib/axios/http.ts
@@ -1,4 +1,5 @@
 import { ServerEnv } from '@/actions/api-client/server-env'
+import { auth } from '@/app/api/auth/[...nextauth]/auth'
 import axios from 'axios'
 
 export const http = axios.create({
@@ -6,4 +7,13 @@ export const http = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+})
+
+http.interceptors.request.use(async (config) => {
+  const session = await auth()
+
+  if (session?.user?.accessToken) {
+    config.headers.Authorization = `Bearer ${session.user.accessToken}`
+  }
+  return config
 })
