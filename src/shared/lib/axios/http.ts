a896e27ae45e51dc8d32efc72fda0d1b4ee6ecb8
류정우 <evencoding@gmail.com>
2024-11-09 01:21:52 +0900

    chore: 불필요한 코드 제거

--- src/shared/lib/axios/http.ts
+++ src/shared/lib/axios/http.ts
@@ -1,5 +1,4 @@
 import { ServerEnv } from '@/actions/api-client/server-env'
-import { auth } from '@/app/api/auth/[...nextauth]/auth'
 import axios from 'axios'
 
 export const http = axios.create({
@@ -7,13 +6,4 @@ export const http = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-})
-
-http.interceptors.request.use(async (config) => {
-  const session = await auth()
-
-  if (session?.user?.accessToken) {
-    config.headers.Authorization = `Bearer ${session.user.accessToken}`
-  }
-  return config
 })
