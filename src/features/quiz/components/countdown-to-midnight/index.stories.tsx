78f881b35a2fbbc15234b4fe7d1adf8e17d660aa
nageuna922@gmail.com <nageuna922@gmail.com>
2024-11-11 18:29:43 +0900

    refactor: 스토리북 수정

--- src/features/quiz/components/countdown-to-midnight/index.stories.tsx
+++ src/features/quiz/components/countdown-to-midnight/index.stories.tsx
@@ -6,13 +6,19 @@ const meta = {
   component: CountdownToMidnight,
   tags: ['autodocs'],
   parameters: {
-    layout: 'centered',
     docs: {
       description: {
         component: '오늘의 퀴즈가 도착하는 자정까지 남은 시간을 표시하는 컴포넌트입니다.',
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div className="mx-auto max-w-mobile p-4">
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof CountdownToMidnight>
 
 export default meta
