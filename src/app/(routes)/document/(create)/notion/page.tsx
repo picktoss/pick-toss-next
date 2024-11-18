fa8f27e4505917b6c223243006e9fffafddcb8ff
nageuna922@gmail.com <nageuna922@gmail.com>
2024-11-18 22:09:38 +0900

    feat: notion view component

--- src/app/(routes)/document/(create)/notion/page.tsx
+++ src/app/(routes)/document/(create)/notion/page.tsx
@@ -1,26 +1,9 @@
 import NotionDocument from '@/features/document/screens/notion-document'
-import NewQuizDrawer from '@/features/quiz/components/new-quiz-drawer'
-import FixedBottom from '@/shared/components/custom/fixed-bottom'
-import { Button } from '@/shared/components/ui/button'
 
 const CreateWithNotionPage = () => {
   return (
     <>
       <NotionDocument />
-
-      <FixedBottom className="px-[20px]">
-        <NewQuizDrawer
-          triggerComponent={
-            <Button
-              variant={'largeRound'}
-              colors={'primary'}
-              className="flex-center h-[52px] w-full"
-            >
-              퀴즈 만들기
-            </Button>
-          }
-        />
-      </FixedBottom>
     </>
   )
 }
--- src/features/document/screens/notion-document.tsx
+++ src/features/document/screens/notion-document.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import { EditDocumentProvider } from '@/features/editor/context/edit-document-context'
+import NewQuizDrawer from '@/features/quiz/components/new-quiz-drawer'
 import {
   useNotionAuthMutation,
   useGetNotionMdFile,
   useGetNotionPages,
 } from '@/requests/notion/hooks'
+import FixedBottom from '@/shared/components/custom/fixed-bottom'
 import Icon from '@/shared/components/custom/icon'
 import Loading from '@/shared/components/custom/loading'
+import { Button } from '@/shared/components/ui/button'
 import Text from '@/shared/components/ui/text'
 import { countPlainTextCharacters } from '@/shared/utils/text'
 import { useEffect, useState } from 'react'
@@ -61,44 +64,60 @@ const NotionDocument = () => {
       {!notionMdData || isReconnect ? (
         <Loading center />
       ) : (
-        <EditDocumentProvider>
-          <div className="fixed top-[54px] flex w-full max-w-mobile items-center justify-between bg-background-base-02 px-[16px] py-[11px]">
-            <div className="flex items-center">
-              <Icon name="info" className="mr-[8px] size-[16px]" />
-              <Text as="span" typography="text2-medium" className="mr-[4px] text-text-secondary">
-                연결한 페이지: {title}
-                {pageCount > 1 && (
-                  <>
-                    {' 외 '} {pageCount - 1} {' 개'}
-                  </>
-                )}
-              </Text>
-              <Text as="span" typography="text2-medium" className="text-text-caption">
-                {characterCount}
-              </Text>
+        <>
+          <EditDocumentProvider>
+            <div className="fixed top-[54px] flex w-full max-w-mobile items-center justify-between bg-background-base-02 px-[16px] py-[11px]">
+              <div className="flex items-center">
+                <Icon name="info" className="mr-[8px] size-[16px]" />
+                <Text as="span" typography="text2-medium" className="mr-[4px] text-text-secondary">
+                  연결한 페이지: {title}
+                  {pageCount > 1 && (
+                    <>
+                      {' 외 '} {pageCount - 1} {' 개'}
+                    </>
+                  )}
+                </Text>
+                <Text as="span" typography="text2-medium" className="text-text-caption">
+                  {characterCount}
+                </Text>
+              </div>
+              <button
+                onClick={() => {
+                  setIsReconnect(true)
+                  notionAuth()
+                }} // 변경 버튼 클릭 시 노션 연동 로직 다시 수행
+                className="text-text1-medium text-button-text-tertiary"
+              >
+                <Text typography="text1-medium" className="text-text-secondary">
+                  변경
+                </Text>
+              </button>
             </div>
-            <button
-              onClick={() => {
-                setIsReconnect(true)
-                notionAuth()
-              }} // 변경 버튼 클릭 시 노션 연동 로직 다시 수행
-              className="text-text1-medium text-button-text-tertiary"
-            >
-              <Text typography="text1-medium" className="text-text-secondary">
-                변경
-              </Text>
-            </button>
-          </div>
 
-          <div className="mb-[100px] mt-[97px] size-full">
-            <h2 className="w-full px-[16px] py-[24px] align-middle text-title2 text-text-placeholder-02">
-              {title}
-            </h2>
-            <div className="px-[20px] pb-[10px] pt-[4px] text-text-disabled">
-              {notionMdData?.content}
+            <div className="mb-[100px] mt-[97px] size-full">
+              <h2 className="w-full px-[16px] py-[24px] align-middle text-title2 text-text-placeholder-02">
+                {title}
+              </h2>
+              <div className="px-[20px] pb-[10px] pt-[4px] text-text-disabled">
+                {notionMdData?.content}
+              </div>
             </div>
-          </div>
-        </EditDocumentProvider>
+          </EditDocumentProvider>
+
+          <FixedBottom className="px-[20px]">
+            <NewQuizDrawer
+              triggerComponent={
+                <Button
+                  variant={'largeRound'}
+                  colors={'primary'}
+                  className="flex-center h-[52px] w-full"
+                >
+                  퀴즈 만들기
+                </Button>
+              }
+            />
+          </FixedBottom>
+        </>
       )}
     </>
   )
