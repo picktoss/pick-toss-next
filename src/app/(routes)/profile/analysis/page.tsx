f883ae813230558536367e9c632fd360f713cb63
nageuna922@gmail.com <nageuna922@gmail.com>
2024-12-02 20:17:05 +0900

    feat: layout

---
+++ src/app/(routes)/profile/analysis/page.tsx
@@ -0,0 +1,9 @@
+const AnalysisPage = () => {
+  return (
+    <main className="h-[calc(100dvh-54px-88px)] w-full overflow-y-auto">
+      <div className="flex-center flex-col"></div>
+    </main>
+  )
+}
+
+export default AnalysisPage
