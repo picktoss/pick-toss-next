2265d8e7f04d00ad0b0029e3240e0ce0a362c200
GeunaNa <155413929+rabyeoljji@users.noreply.github.com>
2025-03-11 23:08:26 +0900

    feat: 랜딩 페이지 결합 (#329)
    * fix: landing
    
    * fix: landing

--- src/features/view/login-for-landing.tsx
+++ src/features/view/login-for-landing.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import AppStartView from './app-start'
 import { useSession } from 'next-auth/react'
 import Splash from './splash'
+import AppStartView from './app-start'
 
-const Landing = () => {
+const LoginForLanding = () => {
   const { status } = useSession()
 
   if (status === 'loading') {
@@ -14,4 +14,4 @@ const Landing = () => {
   return <AppStartView />
 }
 
-export default Landing
+export default LoginForLanding
